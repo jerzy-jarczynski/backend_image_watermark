88fc4a86b1e18421c79bdd5bd87884f2506f8e09
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-06-29 22:41:07 +0200

    Add jimp modifiers functions. Add conditional dialog.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Jimp = require('jimp');
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
   const image = await Jimp.read(inputFile);
@@ -32,6 +33,74 @@ const prepareOutputFilename = (filename) => {
   return `${name}-with-watermark.${ext}`;
 };
 
+const makeImageBrighter = async (inputFile, outputFile, brightnessValue) => {
+  try {
+    // Read the image using Jimp
+    const image = await Jimp.read(inputFile);
+
+    // Adjust the brightness of the image
+    image.brightness(brightnessValue);
+
+    // Save the modified image to the output path
+    await image.writeAsync(outputFile);
+
+    console.log('Image brightness adjusted successfully.');
+  } catch (error) {
+    console.error('Error adjusting image brightness:', error);
+  }
+};
+
+const increaseImageContrast = async (inputFile, outputFile, contrastValue) => {
+  try {
+    // Read the image using Jimp
+    const image = await Jimp.read(inputFile);
+
+    // Adjust the contrast of the image
+    image.contrast(contrastValue);
+
+    // Save the modified image to the output path
+    await image.writeAsync(outputFile);
+
+    console.log('Image contrast increased successfully.');
+  } catch (error) {
+    console.error('Error increasing image contrast:', error);
+  }
+};
+
+const convertToBlackAndWhite = async (inputFile, outputFile) => {
+  try {
+    // Read the image using Jimp
+    const image = await Jimp.read(inputFile);
+
+    // Convert the image to black and white (grayscale)
+    image.greyscale();
+
+    // Save the modified image to the output path
+    await image.writeAsync(outputFile);
+
+    console.log('Image converted to black and white successfully.');
+  } catch (error) {
+    console.error('Error converting image to black and white:', error);
+  }
+};
+
+const invertImage = async (inputFile, outputFile) => {
+  try {
+    // Read the image using Jimp
+    const image = await Jimp.read(inputFile);
+
+    // Invert the colors of the image
+    image.invert();
+
+    // Save the modified image to the output path
+    await image.writeAsync(outputFile);
+
+    console.log('Image colors inverted successfully.');
+  } catch (error) {
+    console.error('Error inverting image colors:', error);
+  }
+};
+
 const startApp = async () => {
 
   // Ask if user is ready
@@ -51,10 +120,73 @@ const startApp = async () => {
     message: 'What file do you want to mark?',
     default: 'test.jpg',
   }, {
+    name: 'modifyImage',
+    type: 'confirm',
+    message: 'Do you want to modify image?',
+  }, {
+    name: 'imageModifier',
+    type: 'list',
+    choices: [
+      'Make image brighter',
+      'Increase contrast',
+      'Make image B&W',
+      'Invert image',
+    ],
+    when: (answers) => answers.modifyImage === true,
+  }, {
     name: 'watermarkType',
     type: 'list',
     choices: ['Text watermark', 'Image watermark'],
   }]);
+
+  if (options.modifyImage === true) {
+    switch (options.imageModifier) {
+      case 'Make image brighter':
+        try {
+          makeImageBrighter(
+            './img/' + options.inputImage,
+            './img/' + options.inputImage,
+            0.5
+          );
+          break;
+        } catch (error) {
+          console.log('\nSomething went wrong while making the image brighter. Try again.\n');
+        }
+      case 'Increase contrast':
+        try {
+          increaseImageContrast(
+            './img/' + options.inputImage,
+            './img/' + options.inputImage,
+            0.5
+          );
+          break;
+        } catch (error) {
+          console.log('\nSomething went wrong while increasing the image contrast. Try again.\n');
+        }
+      case 'Make image B&W':
+        try {
+          convertToBlackAndWhite(
+            './img/' + options.inputImage,
+            './img/' + options.inputImage
+          );
+          break;
+        } catch (error) {
+          console.log('\nSomething went wrong while converting the image to black and white. Try again.\n');
+        }
+      case 'Invert image':
+        try {
+          invertImage(
+            './img/' + options.inputImage,
+            './img/' + options.inputImage
+          );
+          break;
+        } catch (error) {
+          console.log('\nSomething went wrong while inverting the image. Try again.\n');
+        }
+      default:
+        console.log('Unknown action');
+    }
+  }
 
   if(options.watermarkType === 'Text watermark') {
     const text = await inquirer.prompt([{
@@ -62,8 +194,20 @@ const startApp = async () => {
       type: 'input',
       message: 'Type your watermark text:',
     }])
+
     options.watermarkText = text.value;
-    addTextWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), options.watermarkText);
+
+    if (fs.existsSync('./img/' + options.inputImage)) {
+      try {
+        addTextWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), options.watermarkText);
+        console.log('\nWatermark added successfully\n');
+        startApp();
+      } catch(error) {
+        console.log('\nSomething went wrong... Try again\n');
+      }
+    } else {
+      console.log('\nSomething went wrong... Try again\n');
+    }
   }
   else {
     const image = await inquirer.prompt([{
@@ -73,7 +217,19 @@ const startApp = async () => {
       default: 'logo.png',
     }])
     options.watermarkImage = image.filename;
-    addImageWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), './img/' + options.watermarkImage);
+
+    if (fs.existsSync('./img/' + options.inputImage)
+     && fs.existsSync('./img/' + options.watermarkImage)) {
+      try {
+        addImageWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), './img/' + options.watermarkImage);
+        console.log('\nWatermark added successfully\n');
+        startApp();
+      } catch(error) {
+        console.log('\nSomething went wrong... Try again\n');
+      }
+    } else {
+      console.log('\nSomething went wrong... Try again\n');
+    }
   }
 
 };
